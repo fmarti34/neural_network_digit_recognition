440ce38a936514ab68ca9f3f5bd2775f1674e618
felixmartinez <martinez.j.felix@gmail.com>
2020-11-04 17:44:37 -0600

    added forward pass

--- neural_network.py
+++ neural_network.py
@@ -4,10 +4,11 @@ import numpy as np
 class NeuralNetwork:
     def __init__(self, sizes):
         self.sizes = sizes
+        self.bias = np.random.random()
         self.weights = self.initialize_weights()
 
-    def neural_network(self, input1, input2, w1, w2, bias):
-        return self.sigmoid(input1 * w1 + input2 * w2 + bias)
+    def neural_network(self, inputs, weights):
+        return self.sigmoid(np.dot(weights, inputs) + self.bias)
 
     @staticmethod
     def sigmoid(x):
@@ -19,3 +20,11 @@ class NeuralNetwork:
             print(self.sizes[i-1], self.sizes[i])
             weights[i-1] = np.random.randn(self.sizes[i], self.sizes[i-1]) * np.sqrt(1. / self.sizes[i])
         return weights
+
+    def forward_pass(self, image):
+        layer = image.reshape((28, 28)).reshape(28 ** 2, 1)  # convert image to 784x1 vector
+
+        for i in range(len(self.weights)):
+            layer = self.neural_network(layer, self.weights[i])
+
+
