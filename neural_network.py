347b5a3a3cb3c76ffeccb402db94200756b91ae6
felixmartinez <martinez.j.felix@gmail.com>
2020-11-20 19:49:56 -0600

    Added some documentation

--- neural_network.py
+++ neural_network.py
@@ -7,7 +7,15 @@ DIR = os.getcwd()
 
 
 class NeuralNetwork:
-    def __init__(self, sizes):
+    def __init__(self, sizes, train=False):
+        """
+        :param sizes: list
+            A list of integers used to determine the sizes of the input, hidden, and output layers.
+            ex: [784, 128, 10] : Will initialize a 784x128 matrix of weights and bias. Also, it will initialize a
+                                 128x10 matrix of weights and bias.
+        :param train: boolean, optional
+            When this flag is true it will randomize the biases and weights even if it can load model from disk
+        """
         self.sizes = sizes
         self.learning_rate = 0.01
         self.nodes = {}
@@ -15,7 +23,7 @@ class NeuralNetwork:
         self.trained = False
 
         # set biases and weights to random number if not able to load model from disk
-        if self.load() is False:
+        if train or self.load() is False:
             self.biases = self.initialize_bias()
             self.weights = self.initialize_weights()
         else:
@@ -32,27 +40,58 @@ class NeuralNetwork:
         return (prediction - target) ** 2
 
     def activation(self, inputs, weights, bias):
+        """
+        Calculates the next layer by using the dot product, it multiplies the weights matrix by the inputs matrix
+        connecting them to the neurons in the next layer. Next it adds the bias vector using matrix addition. Lastly,
+        it applies the sigmoid function to the layer.
+
+        :param inputs: numpy.ndarray
+        :param weights: numpy.ndarray
+        :param bias: numpy.ndarray
+        :return: numpy.ndarray
+        """
         return self.sigmoid(np.dot(weights, inputs) + bias)
 
     def sigmoid_derivative(self, x):
-        # return self.sigmoid(x) * (1 - self.sigmoid(x)) - actual sigmoid derivative
-        # since sigmoid is already applied to x in forward_pass()
+        """
+        return self.sigmoid(x) * (1 - self.sigmoid(x)) - actual sigmoid derivative
+
+        since sigmoid is already applied to x in forward_pass() we do not apply sigmoid to x again
+
+        :param x: numpy.ndarray
+        :return: numpy.ndarray
+        """
         return x * (1 - x)
 
     def initialize_weights(self):
+        """
+        Initializes random matrices that will be used as weights. Each weight matrix is added to dictionary with the
+        corresponding index as its key.
+
+        :return: dict
+        """
         weights = {}
         for i in range(1, len(self.sizes)):
             weights[i-1] = np.random.randn(self.sizes[i], self.sizes[i-1]) * np.sqrt(1. / self.sizes[i])
         return weights
 
     def initialize_bias(self):
+        """
+        Initializes random matrices that will be used as biases. Each bias matrix is added to dictionary with the
+        corresponding index as its key.
+
+        :return: dict
+        """
+
         bias = {}
         for i in range(1, len(self.sizes)):
             bias[i-1] = np.random.randn(self.sizes[i], 1) * np.sqrt(1. / self.sizes[i])
         return bias
 
     def save(self):
-        # save weights and biases to disk
+        """
+        save weights and biases to disk in '/model' folder
+        """
         try:
             with open(DIR + '/model/weights.pickle', 'wb') as handle:
                 pickle.dump(self.weights, handle)
@@ -60,7 +99,7 @@ class NeuralNetwork:
             with open(DIR + '/model/bias.pickle', 'wb') as handle:
                 pickle.dump(self.biases, handle)
         except FileNotFoundError as e:
-            print('ERROR: Could not save files successfully!')
+            print('ERROR: Could not save files successfully: ' + e)
 
 
 
@@ -81,6 +120,14 @@ class NeuralNetwork:
         return True
 
     def forward_pass(self, image):
+        """
+        Calculates every layer and stores it in self.nodes dictionary
+
+        :param image: numpy.ndarray
+        :return: numpy.ndarray
+            This is the output layer
+        """
+
         self.nodes[0] = layer = image.reshape((28, 28)).reshape(28 ** 2, 1)  # convert image to 784x1 matrix
 
         for i in range(len(self.weights)):
@@ -90,6 +137,12 @@ class NeuralNetwork:
         return layer
 
     def predict_image(self, image):
+        """
+        Predicts the handwritten digit that's in the image
+
+        :param image: numpy.ndarray
+        :return: numpy.int64
+        """
         return np.argmax(self.forward_pass(image), axis=0)[0]
 
     def update_weights_iter(self, target):
@@ -142,7 +195,7 @@ class NeuralNetwork:
 
         self.update_weights(target)
 
-    def train(self, data, labels, iterations=10):
+    def train(self, data, labels, iterations=5):
         for iter in range(iterations):
             for i in trange(len(data),  desc=str(iter) + '/' + str(iterations)):
                 prediction = self.forward_pass(data[i])
@@ -158,3 +211,4 @@ class NeuralNetwork:
 
                 self.back_propagation(prediction, target)
                 # print(self.nodes)
+        self.save()
